bae407f04537bf079db79f7fde7ce463760b041a
Matthew Pullen <matthew.pullen@gmail.com>
2025-12-05 00:11:52 -0600

    Added dummy PLATFORM JSON

--- lib/models/participant.dart
+++ lib/models/participant.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class Participant {
   final String pid;
   final String study;
+  final DateTime enrollmentDate;
 
-  Participant({required this.pid, required this.study});
+  Participant({
+    required this.pid,
+    required this.study,
+    required this.enrollmentDate,
+  });
 }

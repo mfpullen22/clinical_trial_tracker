754c635f5ed67a1d945430dd8e71b193fd5ac95a
Matthew Pullen <matthew.pullen@gmail.com>
2025-12-05 00:24:25 -0600

    Spaced out buttons on home screen

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
     final theme = context.theme;
     final typography = theme.typography;
     final colors = theme.colors;
+    final screenHeight = MediaQuery.of(context).size.height;
 
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -20,16 +21,27 @@ class HomeScreen extends StatelessWidget {
           style: typography.sm.copyWith(color: colors.mutedForeground),
         ),
         const FDivider(),
-        FButton(
-          onPress: () {},
-          style: FButtonStyle.primary(),
-          child: const Text("Add New Participant"),
+        SizedBox(
+          height: screenHeight * 0.20,
+        ), // 25% of screen height for spacing above
+        SizedBox(
+          height: 60,
+          child: FButton(
+            onPress: () {},
+            style: FButtonStyle.primary(),
+            child: const Text("Add New Participant"),
+          ),
         ),
-        const SizedBox(height: 15),
-        FButton(
-          onPress: () {},
-          style: FButtonStyle.primary(),
-          child: const Text("Manage Current Participant"),
+        SizedBox(
+          height: screenHeight * 0.05,
+        ), // 3% of screen height between buttons
+        SizedBox(
+          height: 60,
+          child: FButton(
+            onPress: () {},
+            style: FButtonStyle.primary(),
+            child: const Text("Manage Current Participant"),
+          ),
         ),
       ],
     );
